ce58d89506fe3868b1aa8c0a19f65e2766db0f8b
Heliflyer <webpurchases@hotmail.co.uk>
2020-12-21 11:48:21 +0000

    Update MQTT Demo code.py
    Fixed bug message, topic to message.topic

--- MQTT Demo code.py
+++ MQTT Demo code.py
@@ -3,7 +3,7 @@ import time
 
 def on_message(client, userdata, message):
 	#print("message received", str(message.payload.decode("utf-8")))
-	print("message topic=", message, topic)
+	print("message topic=", message.topic)
 	#print("message retain flag=",message.retain)
 	
 broker="m20.cloudmqtt.com"
